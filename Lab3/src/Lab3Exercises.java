2320c3d80e0c68a874776499ac3bee21df221e1a
4015e6 <4015e6@guineo.ece.uprm.edu>
2017-02-08 18:31:20 -0400

    Se cambio la posicion de las llaves iniciales.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,7 +1,9 @@
 import javax.swing.JFrame;
 
-public class Lab3Exercises {
-	public static void main(String[] args) {
+public class Lab3Exercises 
+{
+	public static void main(String[] args)
+	{
 		JFrame myFrame = new JFrame("Michael");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 400);
